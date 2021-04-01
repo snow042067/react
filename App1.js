b0d71f714c187a1f261fd8c33ba1e9022c2c5f22
snow042067 <55731206+snow042067@users.noreply.github.com>
2021-04-01 12:03:10 +0800

    Add files via upload

---
+++ App1.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function App() { 
+    return (Header,Content)
+}
+
+function Header() {
+    return (<h1>Header</h1>);
+}
+
+function Content() {
+    return (
+        <div>
+           <h2>Content</h2>
+           <p>The content text!!!</p>
+        </div>
+     );
+}
+
+export default App;
